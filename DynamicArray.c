9c44bf9d5da7a50d58fac4ebd2060a5bb52ec714
Whiskas101 <getintouchwithvic@gmail.com>
2023-05-07 12:26:50 +0530

    FInished all basic array operation implementation
    Conversion to a proper library remains to be completed

--- DynamicArray.c
+++ DynamicArray.c
@@ -9,11 +9,11 @@ typedef struct dArr{
 
     //function pointers
     //these will, well point to functions that can only operate on THIS specific data structure
+    
     void (*append) (struct dArr*, int); //this takes in a dArr, which is just itself, and an integer, that we will add to this arr
     int (*pop)(struct dArr*);
     void (*display)(struct dArr*);
     int (*delete) (struct dArr*, int);
-
 
     //end of functions of dArr
 
@@ -47,7 +47,6 @@ dArr * init_dynamic_Arr(int size){
     self->pop = &pop;
     self->delete = &delete;
     self->display = &display;
-
 
     return self;
 
@@ -96,14 +95,79 @@ void append(dArr* self, int val){
 
 
 // removes the last element in the array, and returns it
-int pop(dArr * self){}
+int pop(dArr * self){
+    int result = 0;
+    // cant pop an item if the list is empty
+    if(self->nSize == 0 || self->LastElementIndex == -1){
+        printf("\n Empty array, can't pop \n");
+    
+    // if the array has more than 0 element s
+    }else{
+        //storing the value to be popped into a result variable, and adjusting the last element index position
+        result = self->data[(self->LastElementIndex)];
+        (self->LastElementIndex) -= 1;
+    }
+
+
+    return result;
+
+}
 
 // Displays horizontally, each element
-void display(dArr * self){}
+void display(dArr * self){
+    
+    //tried to make it look like a python list on output
+    printf("\n");
+    int i = 0;
+    printf("[");
+    while(i <= (self->LastElementIndex)){
+        printf("%d", self->data[i]);
+        printf(", ");
+        i++;
+    }
+    printf("]");
+
+}
 
 // Delete
 // Removes the element at the given index and returns it
-int delete(dArr * self, int index){}
+int delete(dArr * self, int index){
+
+    int result;
+
+    // if the index is out of range of the array, we raise an error
+    if(index > self->LastElementIndex || index < 0){
+        printf("\n Array does not have an element at index %d \n", index);
+        result = 0;
+
+    // if the index to be deleted is the END of the list, we can just call pop() function
+    }else if(index == self->LastElementIndex){
+        result = pop(self);
+        
+        
+    
+    // if the index is in between last and first, we need to SHIFT every element after it to the left by 1
+    }else{
+        //left shifting each element
+        int temp = self->data[index];
+        result = temp;
+
+        while(index + 1 != self->LastElementIndex){
+            self->data[index] = self->data[index + 1]; //we replace the current element with the next element, and repeat this
+            index++;
+        }
+        //we shift the last index to the left, because we deleted an element from the array
+        self->LastElementIndex -= 1;
+
+        
+        
+
+    }
+    
+    return result;
+
+}
+
 
 
 
@@ -114,7 +178,18 @@ int main(){
 
     DYNAMIC_ARRAY->append(DYNAMIC_ARRAY, 1);
     DYNAMIC_ARRAY->append(DYNAMIC_ARRAY, 2);
+    DYNAMIC_ARRAY->append(DYNAMIC_ARRAY, 3);
     DYNAMIC_ARRAY->append(DYNAMIC_ARRAY, 4);
+    DYNAMIC_ARRAY->append(DYNAMIC_ARRAY, 7);
 
-    printf("element : %d", DYNAMIC_ARRAY->data[2]);
+    display(DYNAMIC_ARRAY);
+
+    int deletedVal = delete(DYNAMIC_ARRAY, 0);
+    
+    
+
+    //printf("element : %d", DYNAMIC_ARRAY->data[2]);
+    
+    printf("\n element deleted : %d", deletedVal);
+    display(DYNAMIC_ARRAY);
 }
